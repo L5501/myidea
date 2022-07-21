b1f1b09928b5a9e03df235ad80bdb68672aa5545
li li <2085293532@qq.com>
2022-07-21 18:10:47 +0800

    Initial commit

---
+++ src/main/java/com/ll/myidea/MyideaApplication.java
@@ -0,0 +1,13 @@
+package com.ll.myidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyideaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyideaApplication.class, args);
+    }
+
+}
